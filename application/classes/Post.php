58185ea84c1fc0967c940c97c0ee97dd4c7ee21f
unknown <Bao@BAO-LAPTOP.(none)>
2012-02-09 20:45:01 +0100

    Lagde Post-klasse.

---
+++ application/classes/Post.php
@@ -0,0 +1,62 @@
+<?php
+class Post {
+
+	private $id;
+	private $title;
+	private $date;
+	private $text;
+	private $user_id;
+	private $category_id;
+
+	public function __construct($id, $title, $date, $text, $user_id, $category_id) {
+
+		$this->id = $id;
+		$this->title = $title;
+		$this->date = $date;
+		$this->text = $text;
+		$this->user_id = $user_id;
+		$this->category_id = $category_id;
+	}
+
+	public function get_id() {
+		return $this->id;
+	}
+	public function set_id($id) {
+		$this->id = $id;
+	}
+
+	public function get_title() {
+		return $this->title;
+	}
+	public function set_title($title) {
+		$this->title = $title;
+	}
+
+	public function get_date() {
+		return $this->date;
+	}
+	public function set_date($date) {
+		$this->date = $date;
+	}
+
+	public function get_text() {
+		return $this->text;
+	}
+	public function set_text($text) {
+		$this->text = $text;
+	}
+
+	public function get_user_id() {
+		return $this->user_id;
+	}
+	public function set_user_id($user_id) {
+		$this->user_id = $user_id;
+	}
+	
+	public function get_category_id() {
+		return $this->category_id;
+	}
+	public function set_category_id($category_id) {
+		$this->category_id = $category_id;
+	}
+}
